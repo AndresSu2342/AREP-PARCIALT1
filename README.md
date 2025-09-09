90bbe020ad8fcf33be5664e4e2f7cb5313d842d9
CESAR.BORRAY-S <cesar.borray-s@mail.escuelaing.edu.co>
2025-09-09 13:59:33 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # AREP-PARCIALT1
+
+## Pruebas del sistema Key-Value Store
+
+Este documento describe las pruebas básicas hechas al sistema implementado.
+
+## Pasos realizados
+1. Se ejecutó el servidor fachada en el puerto 36000.
+2. Se accedió al cliente HTML desde el navegador (`http://localhost:36000/`).
+3. Se probó el registro de pares clave-valor con el formulario **SET**.
+4. Se consultaron valores previamente creados con el formulario **GET**.
+
+## Evidencia Pruebas
+En la siguiente imagen se observa el correcto funcionamiento de las operaciones:
+
+![Pantallazo pruebas](https://github.com/user-attachments/assets/077bf997-8dbf-48e4-b3c5-110899d56a72)
+
+![Pantallazo pruebas](https://github.com/user-attachments/assets/8fc5aebb-460a-425e-b23c-7ceea621b251)
+
+## Video Funcionamiento
+
+https://pruebacorreoescuelaingeduco-my.sharepoint.com/:v:/g/personal/cesar_borray-s_mail_escuelaing_edu_co/EQ9eogUG18dJpgZ_pdyRLusBcaVxybHltPUlvOIl6ETOTA?e=DkuJvg&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D
+
+Nota: Despues del parcial lo unico modificado fue este archivo (pruebas y video) y la indexacion del formulario en el servidor fachada
--- src/main/java/org/example/FacadeServer.java
+++ src/main/java/org/example/FacadeServer.java
@@ -26,12 +26,67 @@ public class FacadeServer {
             String path = parts[1];
 
             String backendResponse = forwardToBackend(path);
+            if (path.equals("/") || path.startsWith("/client")) {
+                out.println("HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/html; charset=utf-8\r\n\r\n"
+                        + "<!DOCTYPE html>\n"
+                        + "<html>\n"
+                        + "<head>\n"
+                        + "<meta charset=\"UTF-8\">\n"
+                        + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                        + "<title>Key-Value Store</title>\n"
+                        + "</head>\n"
+                        + "<body>\n"
+                        + "    <h1>Form SET</h1>\n"
+                        + "    <form>\n"
+                        + "        <label for=\"key\">Key:</label><br>\n"
+                        + "        <input type=\"text\" id=\"key\" name=\"key\" value=\"Number\"><br><br>\n"
+                        + "        <label for=\"value\">Value:</label><br>\n"
+                        + "        <input type=\"text\" id=\"value\" name=\"value\" value=\"1234\"><br><br>\n"
+                        + "        <input type=\"button\" value=\"Create\" onclick=\"postGetMsg()\">\n"
+                        + "    </form>\n"
+                        + "    <div id=\"postrespmsg\"></div>\n"
+                        + "    <script>\n"
+                        + "        function postGetMsg() {\n"
+                        + "            let keyVar = document.getElementById(\"key\").value;\n"
+                        + "            let valueVar = document.getElementById(\"value\").value;\n"
+                        + "            const xhttp = new XMLHttpRequest();\n"
+                        + "            xhttp.onload = function () {\n"
+                        + "                document.getElementById(\"postrespmsg\").innerHTML = this.responseText;\n"
+                        + "            }\n"
+                        + "            xhttp.open(\"GET\", \"/setkv?key=\" + keyVar + \"&value=\" + valueVar);\n"
+                        + "            xhttp.send();\n"
+                        + "        }\n"
+                        + "    </script>\n"
+                        + "\n"
+                        + "    <h1>Form GET</h1>\n"
+                        + "    <form>\n"
+                        + "        <label for=\"keyquery\">Key:</label><br>\n"
+                        + "        <input type=\"text\" id=\"keyquery\" name=\"keyquery\" value=\"Number\"><br><br>\n"
+                        + "        <input type=\"button\" value=\"Consultar\" onclick=\"loadGetMsg()\">\n"
+                        + "    </form>\n"
+                        + "    <div id=\"getrespmsg\"></div>\n"
+                        + "    <script>\n"
+                        + "        function loadGetMsg() {\n"
+                        + "            let keyVar = document.getElementById(\"keyquery\").value;\n"
+                        + "            const xhttp = new XMLHttpRequest();\n"
+                        + "            xhttp.onload = function () {\n"
+                        + "                document.getElementById(\"getrespmsg\").innerHTML = this.responseText;\n"
+                        + "            }\n"
+                        + "            xhttp.open(\"GET\", \"/getkv?key=\" + keyVar);\n"
+                        + "            xhttp.send();\n"
+                        + "        }\n"
+                        + "    </script>\n"
+                        + "</body>\n"
+                        + "</html>\n");
 
-            out.println("HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: application/json;"
-                    + "charset=utf-8\r\n\r\n"
-                    + backendResponse);
-
+            }
+            else if (path.startsWith("/setkv") || path.startsWith("/getkv")) {
+                out.println("HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: application/json;"
+                        + "charset=utf-8\r\n\r\n"
+                        + backendResponse);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
